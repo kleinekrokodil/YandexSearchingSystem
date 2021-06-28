f9133d6faae430e7d9d8942b04afcf0183b7451f
Vladimir Kibirev <kaiservladimir@gmail.com>
2021-06-28 18:31:35 +0800

    Итоговый проект по спринту 4, без кучи коммитов:)

---
+++ document.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+
+struct Document {
+    Document();
+    Document(int id_, double relevance_, int rating_);
+    int id = 0;
+    double relevance = 0.0;
+    int rating = 0;
+};
+
+enum class DocumentStatus {
+    ACTUAL,
+    IRRELEVANT,
+    BANNED,
+    REMOVED,
+};
+
